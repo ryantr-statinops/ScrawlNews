9020a65ee0bd3e1afa461b0e644693788564e22c
Ryan Tran <trankhang2856@gmail.com>
2026-09-06 23:20:15 +0700

    fix(gitignore): track frontend rename and untrack node_modules

--- frontend-legacy/node_modules/apexcharts/src/features/weave.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-import ApexCharts from '../apexcharts'
-import WeaveHost from '../modules/weave/WeaveHost'
-
-ApexCharts.registerFeatures({ weave: WeaveHost })
-
-export default ApexCharts
