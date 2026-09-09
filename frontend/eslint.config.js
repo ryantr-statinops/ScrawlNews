f43eb83b11298c8972bcbb0ec9a2396f9ad26e81
Ryan Tran <trankhang2856@gmail.com>
2026-09-09 10:39:10 +0700

    chore(frontend): ignore build output in eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,6 +5,9 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
+  {
+    ignores: ["dist/**"],
+  },
   js.configs.recommended,
   {
     languageOptions: {
